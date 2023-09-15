7aa943069a41b535fbbeabcbd32d14834fd935f9
linhl-2021 <78529608+linhl-2021@users.noreply.github.com>
2023-09-15 14:49:07 +0800

    Add files via upload

---
+++ checkin.py
@@ -0,0 +1,103 @@
+import os
+import requests
+import re
+from urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+def get_keywords(input_str,keyword):
+    # 使用正则表达式查找关键字后面的内容
+    pattern = rf'"{keyword}":"(.*?)"'
+    match = re.search(pattern, input_str)
+
+    if match:
+        extracted_content = match.group(1)
+        # print("提取的内容:", extracted_content)
+        return extracted_content
+    else:
+        print(f"找不到关键字 '{keyword}'")
+
+def get_account(cookie):
+
+    # 要查询的 URL
+    url = 'https://glados.rocks/api/user/status'
+
+    # 设置 User-Agent
+    headers = {
+        'Cookie': cookie,
+        # 'referer': 'https://glados.rocks/console/checkin',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',  # 替换成你自己的 User-Agent 字符串
+    }
+
+    # 发送 POST 请求，传递 payload 数据和请求头
+    response = requests.get(url,headers=headers,verify=False)
+
+    # 检查响应状态码
+    if response.status_code == 200:
+        # 处理响应数据
+        # 请根据实际情况解析响应数据
+        # print(response.text)
+        input_str=response.text
+        keyword1="email"
+        account=get_keywords(input_str,keyword1)
+        return account
+
+def check_in(cookie):
+    # 要查询的 URL
+    url = 'https://glados.rocks/api/user/checkin'
+    # url = 'https://glados.rocks/api/user/status'
+
+    payload = {
+        "token": "glados.one"
+        }
+
+    # 设置 User-Agent
+    headers = {
+        'Cookie': cookie,
+        # 'referer': 'https://glados.rocks/console/checkin',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',  # 替换成你自己的 User-Agent 字符串
+    }
+
+    # 发送 POST 请求，传递 payload 数据和请求头
+    response = requests.post(url,data=payload,headers=headers,verify=False)
+
+    # 检查响应状态码
+    if response.status_code == 200:
+        # 处理响应数据
+        # 请根据实际情况解析响应数据
+        # print(response.text)
+        input_str=response.text
+        keyword1="message"
+        keyword2="business"
+        keyword3="balance"
+
+        #积分
+        balance=get_keywords(input_str,keyword3)
+        #签到时间
+        business=get_keywords(input_str,keyword2)
+        message=get_keywords(input_str,keyword1)
+        business=business.replace("checkin:","")
+        business=business.replace("system:","")
+        return message,business,balance
+
+def test(src_filename,result_filename_csv):
+    with open(result_filename_csv, 'w', encoding='utf-8') as file1:
+            file1.write("账号,积分,签到,信息"+"\n")
+            file1.close()
+    # 打开文件并逐行读取域名
+    with open(src_filename, 'r',encoding='utf-8') as file:
+        for line in file:
+            # 去除行两端的空白字符，然后检查是否为空白行
+            line = line.strip()
+            if line:
+                cookie1=line.replace("\n", "")
+                account=get_account(cookie1)
+                message,business,balance=check_in(cookie1)
+                with open(result_filename_csv, 'a', encoding='utf-8') as file2:
+                    file2.write(f"{account},{balance},{business},{message}\n")
+                    file2.close()
+                    
+codepath = os.path.dirname(os.path.abspath(__file__))
+src_filename=f"{codepath}\\cookie.txt"
+result_filename_csv=f"{codepath}\\result.csv"
+test(src_filename,result_filename_csv)
