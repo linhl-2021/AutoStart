a2879ff7a6d587c7efaa65d24f0fabfb8eedbc01
linhl <405535591@qq.com>
2023-09-19 17:15:33 +0800

    11

--- 应用识别工具/全python海外自动化脚本-96/linhl-auto-py/pycode/bak/main_test.py
+++
@@ -1,8 +0,0 @@
-from config import Config
-from main_test2 import printconfig
-
-# 实例化 Config 类
-config = Config()
-
-print(type(config))
-printconfig(config)
--- 应用识别工具/国内自动化脚本-90/src/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-bash update1.sh $1 $2 $3
-python3 main.py $1 $2 $3
-bash update2.sh $1 $2 $3
